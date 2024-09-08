2b2535f174b078cf104083d44fea2d888c8ed8ee
jatin gurjar <129748129+jatin-gurjar-enjoy@users.noreply.github.com>
2024-09-08 22:46:00 +0530

    Create 1)Display array.cpp

---
+++ 1)Display array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x[6] = {1,4,7,8,3,9};
+    int i;
+    for (i=0;i<6;i++)
+    {
+        cout<<x[i]<<endl;
+    }
+}
+
+/*
+1
+4
+7
+8
+3
+9
+*/
