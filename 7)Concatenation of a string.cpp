89bfc8b1630ec1f2379850a32e90b57c1f14db83
jatin gurjar <129748129+jatin-gurjar-enjoy@users.noreply.github.com>
2024-09-08 22:49:26 +0530

    Create 7)Concatenation of a string.cpp

---
+++ 7)Concatenation of a string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "Jatin";
+    last_name = "Gurjar";
+
+    cout<<first_name+last_name;
+}
+
+/*
+JatinGurjar
+*/
