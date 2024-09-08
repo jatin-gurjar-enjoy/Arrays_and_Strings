b0391ccbd438b81fe88e4fbd1bf573346a0e4e2a
jatin gurjar <129748129+jatin-gurjar-enjoy@users.noreply.github.com>
2024-09-08 22:46:47 +0530

    Create 2)Display user input array.cpp

---
+++ 2)Display user input array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x[6],i,j;
+
+    cout<<"Enter array : ";
+    for(i=0;i<6;i++)
+    {
+        cin>>x[i];   //input from user 
+    }
+    cout<<"The entered elements are: ";
+    for(j=0;j<6;j++)
+    {
+        cout<<x[j]<<endl;  //printing the elements
+    }
+}
+
+/*
+Enter array : 45 67 5 65 43 1
+45
+67
+5
+65
+43
+1
+*/
