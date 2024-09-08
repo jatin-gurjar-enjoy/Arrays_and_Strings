7f669689eca0e6e38773fe950612e365410d86bc
jatin gurjar <129748129+jatin-gurjar-enjoy@users.noreply.github.com>
2024-09-08 22:50:03 +0530

    Create 8)Revesring a string.cpp

---
+++ 8)Revesring a string.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a;
+    cout<<"Enter a string: ";
+    cin>>a;
+    int n = a.length();
+    
+    for(int i=n-1;i>=0;i--)
+    {
+        cout<<a[i];
+    }
+}
+
+/*
+Enter a string: jatin
+nitaj
+*/
